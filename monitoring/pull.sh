8d728cc2c5c79ab834158b73e915d20d98df9812
maartenveldink <maarten.veldink@gmail.com>
2025-05-08 11:46:10 +0200

    Monitoring stack

---
+++ monitoring/pull.sh
@@ -0,0 +1,5 @@
+podman pull docker.io/elasticsearch:8.18.0
+podman pull docker.io/logstash:8.18.0
+podman pull docker.io/kibana:8.18.0
+podman pull docker.io/prom/prometheus:v3.3.1
+podman pull docker.io/grafana/grafana:12.0.0
