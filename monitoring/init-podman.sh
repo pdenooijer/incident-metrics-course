5dd1e9968463820a323cd223cb176e5c8e3e0e87
maartenveldink <maarten.veldink@gmail.com>
2025-05-08 14:23:18 +0200

    ELK stack

--- monitoring/init-podman.sh
+++ monitoring/init-podman.sh
@@ -1,5 +1,10 @@
+#!/bin/zsh
 podman pull docker.io/elasticsearch:8.18.0
 podman pull docker.io/logstash:8.18.0
 podman pull docker.io/kibana:8.18.0
 podman pull docker.io/prom/prometheus:v3.3.1
 podman pull docker.io/grafana/grafana:12.0.0
+
+podman machine stop
+podman machine set --cpus 2 --memory 8048
+podman machine start
