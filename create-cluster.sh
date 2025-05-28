e1c71382b674dc1d164e2aaba1e2d0e7578171bf
maartenveldink <maarten.veldink@gmail.com>
2025-05-28 15:12:35 +0200

    Script voor cluster setup

---
+++ create-cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -Eeu
+podman machine stop
+
+# Create machine and set as default
+podman machine init --memory 16384 incident-metrics-workshop
+podman machine start incident-metrics-workshop
+podman system connection default incident-metrics-workshop
+
+# Create KinD cluster
+KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster --name incident-metrics-workshop
