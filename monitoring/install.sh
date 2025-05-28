fac6ded30ed1e22d1d70273f05df11188eb33c9a
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-05-28 15:41:34 +0200

    Refactor monitoring to k8s solution

---
+++ monitoring/install.sh
@@ -0,0 +1,9 @@
+helm install elasticsearch elastic/elasticsearch -f helm/elasticsearch-values.yml -n monitoring --create-namespace
+helm install filebeat elastic/filebeat -f helm/filebeat-values.yml -n monitoring
+helm install logstash elastic/logstash -f helm/logstash-values.yml -n monitoring
+helm install kibana elastic/kibana -f helm/kibana-values.yml -n monitoring
+
+kubectl get secret elasticsearch-master-credentials -n monitoring -o jsonpath="{.data.username}" | base64 --decode |\
+  xargs printf 'Username: %s \n'
+kubectl get secret elasticsearch-master-credentials -n monitoring -o jsonpath="{.data.password}" | base64 --decode |\
+  xargs printf 'Password: %s \n'
