ea3c77b1531de6fc326ace3c3369fd77ab1b84c3
maartenveldink <maarten.veldink@gmail.com>
2025-05-28 15:14:55 +0200

    Script voor cluster setup

--- create-cluster.sh
+++ create-cluster.sh
@@ -2,7 +2,7 @@
 podman machine stop
 
 # Create machine and set as default
-podman machine init --memory 16384 incident-metrics-workshop
+podman machine init --memory 16384 --cpus 5 incident-metrics-workshop
 podman machine start incident-metrics-workshop
 podman system connection default incident-metrics-workshop
 
