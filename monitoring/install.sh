fa273f946fd42c120348c3836ce0e09c2509e5bf
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-06-11 14:52:28 +0200

    Improve monitoring install script

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -Eeu
 set -o pipefail
 
+monitoring_path=$(dirname "$0")
+
 helm repo add elastic https://helm.elastic.co
 helm repo update elastic
 
-helm install elasticsearch elastic/elasticsearch -f helm/elasticsearch-values.yml -n monitoring --create-namespace
-helm install filebeat elastic/filebeat -f helm/filebeat-values.yml -n monitoring
-helm install logstash elastic/logstash -f helm/logstash-values.yml -n monitoring
-helm install kibana elastic/kibana -f helm/kibana-values.yml -n monitoring
+helm install elasticsearch elastic/elasticsearch -f "$monitoring_path/helm/elasticsearch-values.yml" -n monitoring --create-namespace
+helm install filebeat elastic/filebeat -f "$monitoring_path/helm/filebeat-values.yml" -n monitoring
+helm install logstash elastic/logstash -f "$monitoring_path/helm/logstash-values.yml" -n monitoring
+helm install kibana elastic/kibana -f "$monitoring_path/helm/kibana-values.yml" -n monitoring
 
 kubectl get secret elasticsearch-master-credentials -n monitoring -o jsonpath="{.data.username}" | base64 --decode |\
   xargs printf 'Username: %s \n'
