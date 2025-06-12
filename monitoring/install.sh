40592e9c121f3c7bfb570821b573e3b8a7ef3c6c
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-06-12 11:31:22 +0200

    Improve monitoring setup

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -6,12 +6,15 @@ monitoring_path=$(dirname "$0")
 helm repo add elastic https://helm.elastic.co
 helm repo update elastic
 
-helm install elasticsearch elastic/elasticsearch -f "$monitoring_path/helm/elasticsearch-values.yml" -n monitoring --create-namespace
-helm install filebeat elastic/filebeat -f "$monitoring_path/helm/filebeat-values.yml" -n monitoring
-helm install logstash elastic/logstash -f "$monitoring_path/helm/logstash-values.yml" -n monitoring
-helm install kibana elastic/kibana -f "$monitoring_path/helm/kibana-values.yml" -n monitoring
+helm install elasticsearch elastic/elasticsearch --values "$monitoring_path/helm/elasticsearch-values.yml" \
+  --namespace monitoring --create-namespace
+helm install filebeat elastic/filebeat --values "$monitoring_path/helm/filebeat-values.yml" --namespace monitoring
+helm install logstash elastic/logstash --values "$monitoring_path/helm/logstash-values.yml" --namespace monitoring
+helm install kibana elastic/kibana --values "$monitoring_path/helm/kibana-values.yml" --namespace monitoring
 
-kubectl get secret elasticsearch-master-credentials -n monitoring -o jsonpath="{.data.username}" | base64 --decode |\
-  xargs printf 'Username: %s \n'
-kubectl get secret elasticsearch-master-credentials -n monitoring -o jsonpath="{.data.password}" | base64 --decode |\
-  xargs printf 'Password: %s \n'
+echo
+echo "--- Elasticsearch credentials ---"
+kubectl get secret elasticsearch-master-credentials --namespace monitoring -o jsonpath="{.data.username}" |\
+  base64 --decode | xargs printf 'Username: %s \n'
+kubectl get secret elasticsearch-master-credentials --namespace monitoring -o jsonpath="{.data.password}" |\
+  base64 --decode | xargs printf 'Password: %s \n'
