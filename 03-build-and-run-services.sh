814882681c55eb5015459f2902193b58feb70d09
maartenveldink <maarten.veldink@gmail.com>
2025-10-15 14:28:51 +0200

    Verplaatst naar 1 helm-chart

--- 03-build-and-run-services.sh
+++ 03-build-and-run-services.sh
@@ -1,33 +1,27 @@
 #!/bin/bash -Eeu
 set -o pipefail
 
-
-helm upgrade --install rabbitmq rabbitmq/helm/ --namespace applications --create-namespace
-
-
-mvn package
+#mvn package
 
 registry=localhost:5001
 version=$(date +%s)
-version_tag=${registry}/hello-world:${version}
-podman build hello-world/ --file hello-world/src/main/docker/Dockerfile --tag "${version_tag}"
-podman push "${version_tag}" --tls-verify=false
+
+hello_world_tag=${registry}/hello-world:${version}
+podman build hello-world/ --file hello-world/src/main/docker/Dockerfile --tag "${hello_world_tag}"
+podman push "${hello_world_tag}" --tls-verify=false
 
 helm upgrade --install hello-world hello-world/helm/ --namespace applications --create-namespace \
   --set version="${version}"
 
 
-version_tag=${registry}/rabbitmq-processor:${version}
-podman build rabbitmq-processor/ --file rabbitmq-processor/src/main/docker/Dockerfile --tag "${version_tag}"
-podman push "${version_tag}" --tls-verify=false
-
-helm upgrade --install rabbitmq-processor rabbitmq-processor/helm/ --namespace applications --create-namespace \
-  --set version="${version}"
+processor_tag=${registry}/rabbitmq-processor:${version}
+podman build rabbitmq/rabbitmq-processor/ --file rabbitmq/rabbitmq-processor/src/main/docker/Dockerfile --tag "${processor_tag}"
+podman push "${processor_tag}" --tls-verify=false
 
 
-version_tag=${registry}/rabbitmq-producer:${version}
-podman build rabbitmq-producer/ --file rabbitmq-producer/src/main/docker/Dockerfile --tag "${version_tag}"
-podman push "${version_tag}" --tls-verify=false
+producer_tag=${registry}/rabbitmq-producer:${version}
+podman build rabbitmq/rabbitmq-producer/ --file rabbitmq/rabbitmq-producer/src/main/docker/Dockerfile --tag "${producer_tag}"
+podman push "${producer_tag}" --tls-verify=false
 
-helm upgrade --install rabbitmq-producer rabbitmq-producer/helm/ --namespace applications --create-namespace \
+helm upgrade --install rabbitmq rabbitmq/helm/ --namespace applications --create-namespace \
   --set version="${version}"
