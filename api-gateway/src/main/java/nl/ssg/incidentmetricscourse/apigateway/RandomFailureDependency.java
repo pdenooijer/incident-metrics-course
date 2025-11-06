acc93b641891fec58b564b68d730e5ddcd57a33e
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-11-06 12:47:12 +0100

    Add config property for success rate in api-gateway service

--- api-gateway/src/main/java/nl/ssg/incidentmetricscourse/apigateway/RandomFailureDependency.java
+++ api-gateway/src/main/java/nl/ssg/incidentmetricscourse/apigateway/RandomFailureDependency.java
@@ -2,14 +2,20 @@ package nl.ssg.incidentmetricscourse.apigateway;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.Random;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class RandomFailureDependency {
 
-    Random random = new Random();
+    @ConfigProperty(name = "api-gateway.success-rate")
+    float successRate;
+
+    private final Random random = new Random();
+
 
     public void doSomething() {
-        if (random.nextBoolean()) {
+        boolean randomFailure = random.nextFloat() > successRate;
+        if (randomFailure) {
             throw new RandomFailureException("50/50 random failure");
         }
     }
