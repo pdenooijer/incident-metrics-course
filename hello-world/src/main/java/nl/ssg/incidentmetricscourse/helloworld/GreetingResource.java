14fdf8be8ef090da5f100f923562ce1cd67059c3
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-11-11 17:17:18 +0100

    Add metrics to hello-world app

--- hello-world/src/main/java/nl/ssg/incidentmetricscourse/helloworld/GreetingResource.java
+++ hello-world/src/main/java/nl/ssg/incidentmetricscourse/helloworld/GreetingResource.java
@@ -1,5 +1,6 @@
 package nl.ssg.incidentmetricscourse.helloworld;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,15 +9,23 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    private final MeterRegistry registry;
+
+    GreetingResource(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        registry.counter("hello", "type", "plain").increment();
         return "Hello from Quarkus REST in plain text";
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public HelloWorldResponse helloJson() {
+        registry.counter("hello", "type", "json").increment();
         return new HelloWorldResponse("Hello from Quarkus REST in JSON");
     }
 }
