50d837eeac7fb989084f56124ae45e78f3a264db
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-11-12 09:14:07 +0100

    Make monitoring stack script reinstallable

--- 03-monitoring-stack.sh
+++ 03-monitoring-stack.sh
@@ -7,33 +7,29 @@ helm repo add elastic https://helm.elastic.co
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
 helm repo update elastic prometheus
 
+kubectl delete namespace monitoring --ignore-not-found
+
 echo "--- Installing ---"
 
 echo "Elasticsearch..."
-helm upgrade --install elasticsearch elastic/elasticsearch --namespace monitoring \
+helm install elasticsearch elastic/elasticsearch --namespace monitoring \
   --values "$monitoring_path/elasticsearch-values.yml" --create-namespace
 
 echo "Filebeat..."
-helm upgrade --install filebeat elastic/filebeat --namespace monitoring --values "$monitoring_path/filebeat-values.yml"
+helm install filebeat elastic/filebeat --namespace monitoring --values "$monitoring_path/filebeat-values.yml"
 
 echo "Logstash..."
-helm upgrade --install logstash elastic/logstash --namespace monitoring --values "$monitoring_path/logstash-values.yml"
+helm install logstash elastic/logstash --namespace monitoring --values "$monitoring_path/logstash-values.yml"
 
 echo "Prometheus & Grafana..."
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring \
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring \
   --values "$monitoring_path/prometheus-values.yml"
 
 echo "APM..."
-helm upgrade --install apm-server elastic/apm-server --namespace monitoring --values "$monitoring_path/apm-values.yml"
+helm install apm-server elastic/apm-server --namespace monitoring --values "$monitoring_path/apm-values.yml"
 
 echo "Kibana..."
-kubectl delete secret kibana-kibana-es-token --namespace monitoring --ignore-not-found
-kubectl delete configmap kibana-kibana-helm-scripts --namespace monitoring --ignore-not-found
-kubectl delete serviceaccount pre-install-kibana-kibana --namespace monitoring --ignore-not-found
-kubectl delete role pre-install-kibana-kibana --namespace monitoring --ignore-not-found
-kubectl delete rolebinding pre-install-kibana-kibana --namespace monitoring --ignore-not-found
-kubectl delete job pre-install-kibana-kibana --namespace monitoring --ignore-not-found
-helm upgrade --install kibana elastic/kibana --values "$monitoring_path/kibana-values.yml" --namespace monitoring --force
+helm install kibana elastic/kibana --namespace monitoring --values "$monitoring_path/kibana-values.yml"
 
 echo
 echo "--- Prometheus & Grafana credentials ---"
