583ba79c82adc7a4fdb6dd4c4fb43d086ebec3cd
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-11-12 09:36:17 +0100

    Simplify and improve create machine script
    Don't just throw away the current machine, could be dev environment for
    a customer. Instead throw away the workshop machine if exists.

--- 01-create-machine.sh
+++ 01-create-machine.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo "Stopping current VM..."
-search_string="*"
-podman machine list -q | grep "$search_string" | sed 's/*//g' | xargs -r -n 1 podman machine stop || true
-podman machine list -q | grep "$search_string" | sed 's/*//g' | xargs -r -n 1 podman machine rm --force || true
-
-# Create podman machine and set as default
 name=incident-metrics-workshop
 
-if podman machine ls -q | grep -q "^$name" ; then
-  echo "VM $name already exists..."
-else
-  echo "Creating VM $name..."
-  podman machine init --memory 16384 --cpus 6 "${name}"
-fi
+echo "Stopping and deleting ${name} VM (if exists)..."
+podman machine stop "${name}" || true
+podman machine rm --force "${name}" || true
+
+echo "Creating VM ${name}..."
+podman machine init --memory 16384 --cpus 6 "${name}"
 echo "Starting VM..."
 podman machine start "${name}"
 echo "Setting default VM..."
 podman system connection default "${name}"
 
 registry_name=kind-registry
-if podman container exists "$registry_name" ; then
-  echo "$registry_name already exists, starting it..."
-  podman start "$registry_name"
-else
-  echo "Set up $registry_name and starting it..."
-  # Create registry container
-  # https://kind.sigs.k8s.io/docs/user/local-registry/
-  podman run --detach --restart=always --publish "127.0.0.1:5001:5000" --network bridge --name "$registry_name" registry:2
-fi
+echo "Set up ${registry_name} and starting it..."
+# Create registry container
+# https://kind.sigs.k8s.io/docs/user/local-registry/
+podman run --detach --restart=always --publish "127.0.0.1:5001:5000" --network bridge --name "${registry_name}" registry:2
