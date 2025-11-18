f6f626e2d1d9866fcf4824935219700c20696328
Piet de Nooijer <Piet.deNooijer@soprasteria.com>
2025-11-18 10:44:51 +0100

    Fix podman clock sync script

--- podman_clock_sync.sh
+++ podman_clock_sync.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-podman machine ssh sudo date --set $(date +'%Y-%m-%dT%H:%M:%S')
+
+podman machine ssh incident-metrics-workshop sudo date --set "$(date +'%Y-%m-%dT%H:%M:%S')"
